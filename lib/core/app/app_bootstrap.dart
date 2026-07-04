d47e621631730c911b2fe7cb60f42aa6e000e691
danilodequeiroz <danilo.aq@gmail.com>
2026-07-03 23:47:22 -0300

    moves initialization functions to app_bootstrap.dart.

---
+++ lib/core/app/app_bootstrap.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_gymapp/firebase_options.dart';
+import 'package:logging/logging.dart';
+import 'package:logging_to_logcat/logging_to_logcat.dart';
+
+class AppBootstrap {
+
+  static Future<void> initialize() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await Future.wait([
+      _initLogging(),
+      Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform
+      ),
+      _initLocalStorage(),
+      _initDependencyInjection(),
+    ]);
+  }
+
+  static Future<void> _initDependencyInjection() async {
+  }
+
+  static Future<void> _initLocalStorage() async {
+  }
+
+  static Future _initLogging() async {
+    Logger.root.activateLogcat();
+    Logger.root.level = Level.ALL;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gymapp/core/app/app_bootstrap.dart';
 import 'package:flutter_gymapp/core/localization/l10n/app_localizations.dart';
 import 'package:flutter_gymapp/feature/authentication/presentation/ui_screen_authentication.dart';
 import 'package:flutter_gymapp/feature/training/creation/presentation/ui_screen_gym_exercise.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:logging_to_logcat/logging_to_logcat.dart';
-import 'package:logging/logging.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 const mainScreen = "auth";
 
 void main() async {
-  initLogging();
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await AppBootstrap.initialize();
 
   runApp(const MyApp());
-}
-
-Future initLogging() async {
-  Logger.root.activateLogcat();
-  Logger.root.level = Level.ALL;
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(scaffoldBackgroundColor: Colors.pink),
       navigatorKey: navigatorKey,
       localizationsDelegates: const [
-        AppLocalizations.delegate, // Add this line
+        AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
